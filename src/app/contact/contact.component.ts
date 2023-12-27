9899f00cc4918138eb425ccc54b307e22f35cf7f
Marcin <marcin26012000@gmail.com>
2023-12-27 14:58:18 +0100

    Add contact page layout

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope, faPerson } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  faEnvelope = faEnvelope;
+  faPerson = faPerson;
 }
